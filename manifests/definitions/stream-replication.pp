547a3438682ac77d9b8f569c79a67d30257151e3
root <root@dev.betnet.fr>
2011-05-16 17:46:24 +0200

    Added new resources to set up SR.

---
+++ manifests/definitions/stream-replication.pp
@@ -0,0 +1,64 @@
+/*
+
+define: postgres::stream-replication::master
+
+*/
+define postgres::stream-replication::master(
+$version='9.0',
+$wal_directory='/var/lib/postgresql-wal'
+) {
+  # Create wal directory
+  file {
+    $wal_directory:
+      owner	=> 'postgres',
+      group	=> 'postgres',
+      mode	=> 0700,
+      ensure	=> directory;
+  }
+
+  # PostgreSQL master SR Configuration
+  common::concatfilepart {
+    "postgresql-conf-010-sr-master":
+      require	=> Package['postgresql'],
+      notify	=> Service['postgresql'],
+      file      => "/etc/postgresql/${version}/main/postgresql.conf",
+      manage    => true,
+      content	=> template('postgresql/sr-master.conf.erb');
+  }
+}
+
+define postgres::stream-replication::slave(
+$version='9.0',
+$master_host,
+$master_port='5432',
+$restore_command=false,
+$wal_directory='/var/lib/postgresql-wal'
+) {
+  if $restore_command {
+    $real_restore_command=$restore_command
+  } else {
+    # By default, use RSYNC to copy WAL files from master
+    $real_restore_command="/usr/bin/rsync postgres@${master_host}:${wal_directory}/%f \"%p\""
+  }  
+
+  # PostgreSQL slave SR Configuration
+  common::concatfilepart {
+    "postgresql-conf-010-sr-slave":
+      require	=> Package['postgresql'],
+      notify	=> Service['postgresql'],
+      file      => "/etc/postgresql/${version}/main/postgresql.conf",
+      manage    => true,
+      content	=> template('postgresql/sr-slave.conf.erb');
+  }
+
+  # recovery.conf file
+  file {
+    "/var/lib/postgresql/9.0/main/recovery.conf":
+      owner	=> 'postgres',
+      group	=> 'postgres',
+      mode	=> 0400,
+      require   => Package['postgresql'],
+      notify    => Service['postgresql'],
+      content	=> template('postgresql/recovery.conf.erb');
+  }
+}
